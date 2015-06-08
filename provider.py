ecd56ab47d3db21892c2ca49b3575069dd669ea5
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-06-08 15:44:59 +0200

    provider: Generate access tokens with JWT

--- provider.py
+++ provider.py
@@ -1,9 +1,26 @@
+import time
+
 from flask.ext.oauthlib.provider import OAuth2Provider
 from oauthlib.oauth2.rfc6749.tokens import random_token_generator
+
+import jwt
 
 
 class MyProvider(OAuth2Provider):
     def init_app(self, app):
         super(MyProvider, self).init_app(app)
-        app.config.setdefault('OAUTH2_PROVIDER_TOKEN_GENERATOR', random_token_generator)
+        app.config.setdefault('OAUTH2_PROVIDER_TOKEN_GENERATOR', self.generate_token)
         app.config.setdefault('OAUTH2_PROVIDER_REFRESH_TOKEN_GENERATOR', random_token_generator)
+
+        self.secret = app.config.get('SECRET_KEY')
+
+    def generate_token(self, request):
+        token = {
+            'user': request.user.id,
+            'exp': int(time.time() + request.expires_in),
+        }
+
+        if request.scopes is not None:
+            token['scope'] = ' '.join(request.scopes)
+
+        return jwt.encode(token, self.secret)
