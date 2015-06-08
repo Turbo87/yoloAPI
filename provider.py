9ff8466071e97c928e5a8d8213d7b81813981cda
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-06-08 15:28:15 +0200

    provider: Set token generators to random

--- provider.py
+++ provider.py
@@ -1,6 +1,9 @@
 from flask.ext.oauthlib.provider import OAuth2Provider
+from oauthlib.oauth2.rfc6749.tokens import random_token_generator
 
 
 class MyProvider(OAuth2Provider):
     def init_app(self, app):
         super(MyProvider, self).init_app(app)
+        app.config.setdefault('OAUTH2_PROVIDER_TOKEN_GENERATOR', random_token_generator)
+        app.config.setdefault('OAUTH2_PROVIDER_REFRESH_TOKEN_GENERATOR', random_token_generator)
